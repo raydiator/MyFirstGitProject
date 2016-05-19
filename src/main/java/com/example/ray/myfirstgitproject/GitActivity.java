1b88e1390962a931a629017b2a366f42c4b15120
Ray Dodd <ramendodd@gmail.com>
2016-05-18 19:45:07 -0700

    another comment added

--- src/main/java/com/example/ray/myfirstgitproject/GitActivity.java
+++ src/main/java/com/example/ray/myfirstgitproject/GitActivity.java
@@ -10,5 +10,6 @@ public class GitActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_git);
         //my first git push!
+        //my second
     }
 }
